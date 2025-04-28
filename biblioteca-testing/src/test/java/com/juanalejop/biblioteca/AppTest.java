278de35e71ea2636ce46ca35bf27376104a7c84b
JuanAlejoP <j.patino@alumno.um.edu.ar>
2025-04-28 17:09:46 -0300

    feature/2-set-up-maven: actualizar POM a JUnit5+Mockito y corregir paquete

---
+++ biblioteca-testing/src/test/java/com/juanalejop/biblioteca/AppTest.java
@@ -0,0 +1,15 @@
+package com.juanalejop.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true, "Este test siempre pasa");
+    }
+}
